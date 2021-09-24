f367b36534a601e639c541f3e1a95d826e5d44e1
SubErguotou <fks12301818@163.com>
2021-09-24 19:01:55 +0800

    tag页面样式中版

--- src/components/Tag.vue
+++ src/components/Tag.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
-      <div class="grid grid-cols-1 sm:grid-cols-2 mt-8 md:mt-16">
-        <div class="pr-8 text-2xl font-extrabold font-serif md:text-4xl md:place-self-end">
+      <div class="grid grid-cols-1 sm:grid-cols-4 mt-8 md:mt-16">
+        <div class="sm:col-start-2 sm:col-end-2 pr-8 text-2xl font-extrabold font-serif md:text-4xl md:place-self-end">
           Tags
         </div>
-        <div class="place-self-start pl-8 border-gray-200 md:border-l-2">
-          <span>slot-scope=""</span>
-          <span></span>
-          <span></span>
+        <div class="flex flex-wrap content-center sm:col-start-3 sm:col-end-3 sm:pl-8 border-gray-200 md:border-l-2">
+          <span class="" v-for="(item, index) in tags" :key="index">{{item}}</span>
         </div>
       </div>
   </div>
 </template>
 
 <script>
+import {ref} from "vue"
 export default {
-
+  setup() {
+    const tags = ref(["life", "code", "node"])
+    return {
+      tags
+    }
+  }
 }
 </script>
 
